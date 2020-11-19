ff443ff190aaf9c689ff1fc49a537256777f082c
sajohn <sajohn@users.noreply.github.com>
2020-11-19 01:27:21 -0800

    Update SubArrSumK2.c

--- subarrSumK/SubArrSumK2.c
+++ subarrSumK/SubArrSumK2.c
@@ -1,45 +1,57 @@
 
 
+//Like java solution we need to define find 
+
 struct hashNode {
     int key;
     int val;
     UT_hash_handle hh;
 };
-struct hashNode *g_users = NULL;
-struct hashNode *findUser(int key) {
+
+struct hashNode *findUser(struct hashNode **g_users, int key) {
     struct hashNode *s;
-    HASH_FIND_INT(g_users, &key, s);
+    HASH_FIND_INT(*g_users, &key, s);
     return s;
 }
-int getUserVal(int key) {
+int GetUserVal( struct hashNode **g_users, int key) {
     struct hashNode *s;
-    HASH_FIND_INT(g_users, &key, s);
+    HASH_FIND_INT(*g_users, &key, s);
     return s->val;
 }
-void addUser(int key) {
+void AddUser(struct hashNode **g_users, int key) {
     struct hashNode *s;
-    HASH_FIND_INT(g_users, &key, s);
+    HASH_FIND_INT(*g_users, &key, s);
     if (s == NULL) {
         struct hashNode *s = (struct hashNode *)malloc(sizeof(struct hashNode));
         s->key = key;
         s->val = 1;
-        HASH_ADD_INT(g_users, key, s);
+        HASH_ADD_INT(*g_users, key, s);
     } else {
         s->val++;
     }    
 }
 int subarraySum(int* nums, int numsSize, int k){
+  
+    struct hashNode *g_users = NULL;
+    
     int sum = 0, cnt = 0;
     g_users = NULL;
-    addUser(0);
+    //The first 0 is added to cover the case of target == sum == nums[i]
+    AddUser(&g_users,0);
     for (int i = 0; i < numsSize; i++) {
         sum += nums[i];
-        if (findUser(sum - k)) {
-            cnt += getUserVal(sum-k);
+        if (findUser(&g_users, sum - k)) {
+            cnt += GetUserVal(&g_users, sum-k);
         }
-        addUser(sum);
+        AddUser(&g_users, sum);
     }
+    
+     struct hashNode *current_user, *tmp;
+     HASH_ITER(hh, g_users, current_user, tmp) {
+    HASH_DEL(g_users, current_user);  /* delete it (users advances to next) */
+    free(current_user);             /* free it */
+  }
+    
     return cnt;
 }
-
 
