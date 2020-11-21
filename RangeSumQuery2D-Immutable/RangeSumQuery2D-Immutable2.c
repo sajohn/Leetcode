4199694aa44de74e2c2c734a8292445f486c53cf
sajohn <sajohn@users.noreply.github.com>
2020-11-21 00:42:19 -0800

    Update RangeSumQuery2D-Immutable2.c

--- RangeSumQuery2D-Immutable/RangeSumQuery2D-Immutable2.c
+++ RangeSumQuery2D-Immutable/RangeSumQuery2D-Immutable2.c
@@ -1,8 +1,11 @@
 
+
+
+
 typedef struct {
     int row;
     int col;
-    int **matrix;
+    int **sum;
 } NumMatrix;
 
 bool isValid(int row1, int col1, int row2, int col2, NumMatrix* obj)
@@ -26,37 +29,48 @@ NumMatrix* numMatrixCreate(int** matrix, int matrixSize, int* matrixColSize) {
    NumMatrix* obj = (NumMatrix*)malloc(sizeof(NumMatrix));
     if(!obj)
         return NULL;
-    
+ 
     obj->row = matrixSize;
     obj->col = *matrixColSize;
-    obj->matrix = matrix;
+    obj->sum =  (int**)calloc(matrixSize, sizeof(int*));
     
+    if(obj->sum == NULL)
+        return NULL;
+    
+     for(int i= 0; i<matrixSize; i++)
+     { 
+         obj->sum[i] = (int*)calloc(*matrixColSize+1, sizeof(int));
+        if(obj->sum[i] == NULL)
+             return NULL;
+        for(int j = 0; j<*matrixColSize; j++)
+              obj->sum[i][j+1] = obj->sum[i][j]+matrix[i][j];
+
+     }
    return obj;
 }
 
 int numMatrixSumRegion(NumMatrix* obj, int row1, int col1, int row2, int col2) {
     
     if(obj == NULL || row1 > row2 || col1> col2)
-    {
         return 0;
-    }
+    
   int sum =0; 
-    //if(isValid(row1, col1, row2, col2, obj))
-    {
+    
            for(int i= row1; i<=row2; i++)
-               for(int j = col1; j<=col2; j++)
-               {
-                   //printf("% d ",obj->matrix[i][j]);
-                   sum += obj->matrix[i][j];
-               }
-               
-    }
+                   sum += (obj->sum[i][col2+1] - obj->sum[i][col1]) ;
+
     return sum;
 }
 
 void numMatrixFree(NumMatrix* obj) {
     if(obj)
-        free(obj);
+    {
+        int count =0;
+        while(count <obj->row)
+            free(obj->sum[count++]);
+        
+        free(obj->sum);
+    }
     obj = NULL;
 }
 
