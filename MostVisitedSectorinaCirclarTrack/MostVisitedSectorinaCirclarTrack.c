4e6cc215ca2e312e8a1afc381076ef61f1dc83e8
Sudhansu A John <s_john@apple.com>
2020-12-05 16:12:07 -0800

    https://leetcode.com/problems/most-visited-sector-in-a-circular-track/

---
+++ MostVisitedSectorinaCirclarTrack/MostVisitedSectorinaCirclarTrack.c
@@ -0,0 +1,85 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+struct Count
+{
+    int count; 
+    int sector;
+};
+
+int cmpfunc(void* a, void* b)
+{
+    struct Count* x = (struct Count*)a;
+    struct Count* y = (struct Count*)b;
+    
+    return (y->count - x->count);
+}
+int cmpfuncInt(int* a, int* b)
+{
+    return (*a - *b);
+}
+int* mostVisited(int n, int* rounds, int roundsSize, int* returnSize)
+{
+    *returnSize =0;
+    if(round == NULL || roundsSize ==0)
+        return NULL;
+    struct Count count[n]; 
+    
+    int max = INT_MIN, tmp1= 0, tmp2 =0;
+    
+    memset(count, 0, sizeof(struct Count)*n);
+    
+    int* ret = (int*)calloc(n, sizeof(int));
+    if(ret == NULL)
+        return NULL;
+    
+    for(int i = 0; i < roundsSize-1; i++)
+    {
+        tmp1 = rounds[i];
+        tmp2 = rounds[i+1];
+        while(tmp1%n !=tmp2%n )
+        {
+            count[tmp1%n].count++;
+            count[tmp1%n].sector = (tmp1%n == 0? n:tmp1%n);
+            tmp1++;
+        }
+        
+    }
+    //Last one
+    count[tmp2%n].count++;
+    count[tmp2%n].sector = (tmp2%n == 0? n:tmp2%n);
+    
+     qsort(&count, n, sizeof(struct Count), cmpfunc);
+       max = count[0].count;
+ 
+    int k =0;
+    bool fix = false;
+    for(int i = 0; i < n; i++)
+    {
+        if(count[i].count >=max)
+        {
+            if(count[i].sector == n)
+            {
+             fix = true;
+             continue;
+            }
+            else
+            {
+                ret[k++] = count[i].sector;
+                 (*returnSize)++;
+            }
+           
+        }
+        
+    }
+    if(fix)
+    {
+        ret[k] = n;
+           (*returnSize)++;
+    }
+
+    return ret;
+}
+
---
+++ MostVisitedSectorinaCirclarTrack/ReadMe.txt
@@ -0,0 +1,56 @@
+1560. Most Visited Sector in a Circular Track
+Easy
+
+98
+
+237
+
+Add to List
+
+Share
+Given an integer n and an integer array rounds. We have a circular track which consists of n sectors labeled from 1 to n. A marathon will be held on this track, the marathon consists of m rounds. The ith round starts at sector rounds[i - 1] and ends at sector rounds[i]. For example, round 1 starts at sector rounds[0] and ends at sector rounds[1]
+
+Return an array of the most visited sectors sorted in ascending order.
+
+Notice that you circulate the track in ascending order of sector numbers in the counter-clockwise direction (See the first example).
+
+ 
+
+Example 1:
+
+
+Input: n = 4, rounds = [1,3,1,2]
+Output: [1,2]
+Explanation: The marathon starts at sector 1. The order of the visited sectors is as follows:
+1 --> 2 --> 3 (end of round 1) --> 4 --> 1 (end of round 2) --> 2 (end of round 3 and the marathon)
+We can see that both sectors 1 and 2 are visited twice and they are the most visited sectors. Sectors 3 and 4 are visited only once.
+Example 2:
+
+Input: n = 2, rounds = [2,1,2,1,2,1,2,1,2]
+Output: [2]
+Example 3:
+
+Input: n = 7, rounds = [1,3,5,7]
+Output: [1,2,3,4,5,6,7]
+ 
+
+Constraints:
+
+2 <= n <= 100
+1 <= m <= 100
+rounds.length == m + 1
+1 <= rounds[i] <= n
+rounds[i] != rounds[i + 1] for 0 <= i < m
+Accepted
+12,727
+Submissions
+22,352
+Seen this question in a real interview before?
+
+Yes
+
+No
+For each round increment the visits of the sectors visited during the marathon with 1.
+Determine the max number of visits, and return any sector visited the max number of visits.
+
+
