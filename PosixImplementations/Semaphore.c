ba653c14140398c8e4c4ea65e77d0a14bf660eba
Sudhansu A John <s_john@apple.com>
2021-05-27 17:46:36 -0700

    Posix examples

---
+++ PosixImplementations/Semaphore.c
@@ -0,0 +1,76 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NITER 1000000
+
+
+int cnt = 0;
+
+sem_t mutex; 
+
+void * Count(void * a)
+{
+    int i, tmp;
+    
+    sem_wait(&mutex);
+    for(i = 0; i < NITER; i++)
+    {
+        tmp = cnt;      /* copy the global cnt locally */
+        tmp = tmp+1;    /* increment the local copy */
+        cnt = tmp;      /* store the local value into the global cnt */ 
+    }
+    
+    sem_post(&mutex);
+}
+
+int main(int argc, char * argv[])
+{
+    
+    sem_init(&mutex, 0, 1);
+    
+    pthread_t tid1, tid2;
+
+    if(pthread_create(&tid1, NULL, Count, NULL))
+    {
+      printf("\n ERROR creating thread 1");
+      exit(1);
+    }
+
+    if(pthread_create(&tid2, NULL, Count, NULL))
+    {
+      printf("\n ERROR creating thread 2");
+      exit(1);
+    }
+
+    if(pthread_join(tid1, NULL))	/* wait for the thread 1 to finish */
+    {
+      printf("\n ERROR joining thread");
+      exit(1);
+    }
+
+    if(pthread_join(tid2, NULL))        /* wait for the thread 2 to finish */
+    {
+      printf("\n ERROR joining thread");
+      exit(1);
+    }
+
+    if (cnt < 2 * NITER) 
+        printf("\n BOOM! cnt is [%d], should be %d\n", cnt, 2*NITER);
+    else
+        printf("\n OK! cnt is [%d]\n", cnt);
+        
+
+    sem_destroy(&mutex);
+    pthread_exit(NULL);
+}
+
