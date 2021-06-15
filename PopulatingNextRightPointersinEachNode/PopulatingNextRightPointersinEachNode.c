d00293299ab92f5c2f23c502e6078e07e8f7e256
Sudhansu A John <s_john@apple.com>
2021-06-14 18:11:09 -0700

    https://leetcode.com/problems/populating-next-right-pointers-in-each-node/

---
+++ PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.c
@@ -0,0 +1,66 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct Node *left;
+ *     struct Node *right;
+ *     struct Node *next;
+ * };
+ */
+
+
+#define SIZE 5000
+struct Node* queue[SIZE] = {0};
+
+
+struct Node* connect(struct Node* root){
+
+       if(root==NULL) 
+           return NULL;
+    
+    struct Node *pre = NULL, *original = NULL;
+    
+    
+     if(root!= NULL && (root->left == NULL && root->right == NULL))
+     {
+         root->left = NULL; 
+         root->right = NULL; 
+         root->next = NULL; 
+         return root; 
+     }
+    
+    int head =0, tail = 0; 
+    original = root; 
+    tail = head;
+    queue[head] = root;
+    root->next = NULL; 
+    tail++;
+    queue[tail++] = NULL;
+    
+    while(head<tail)
+    {
+        root = queue[head++];
+       
+        if(root!= NULL) {
+            
+            root->next = queue[head];
+             pre =  root;
+             if(root->left)
+                queue[tail++] = root->left;
+              if(root->right)
+                queue[tail++] = root->right;
+        } else {
+             pre->next = NULL; 
+           if(head == tail) { //Because tail is always pointing to write index and this is last entry so stop
+          
+               break;
+           } else {
+               queue[tail++] = NULL;
+              
+            }
+        }
+    }
+
+    return original;
+
+}
