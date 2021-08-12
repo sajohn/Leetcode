333735ecfce961de607870aaea194645ffb257e9
Sudhansu A John <s_john@apple.com>
2021-08-12 01:30:03 -0700

    https://leetcode.com/problems/combination-sum/

---
+++ CombinationSum/CombinationSum.c
@@ -0,0 +1,53 @@
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+void Dfs(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes, int** ret, int* cur, int idx, int curidx) {
+    
+    if(candidates == NULL || candidatesSize == 0 || returnSize == NULL || returnColumnSizes == NULL)
+        return ; 
+    
+    if(target <= 0 ) {
+       if(target == 0)
+       {
+           ret[*returnSize] = (int*)calloc(curidx, sizeof(int));
+           memcpy(ret[*returnSize], cur, sizeof(int)*(curidx));
+           (*returnColumnSizes)[(*returnSize)++] = curidx;  
+       }
+            return; 
+    }
+
+    for(int i =idx; i < candidatesSize; i++)
+    {
+        cur[curidx++] = candidates[i];    
+        Dfs(candidates, candidatesSize, target- candidates[i], returnSize, returnColumnSizes, ret, cur, i, curidx);
+        curidx--;
+        
+    }
+  
+}
+
+#define MAX 150
+int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
+    *returnSize = 0; 
+    if(candidates == NULL || candidatesSize == 0 || returnSize == NULL || returnColumnSizes == NULL)
+        return NULL; 
+    
+    int** ret = (int**) calloc(MAX, sizeof(int*));
+    if(ret == NULL)
+        return NULL; 
+    *returnColumnSizes = (int*) calloc(MAX, sizeof(int));
+    if(returnColumnSizes == NULL)
+        return NULL; 
+
+    int* cur = (int*)calloc(MAX, sizeof(int)); 
+    Dfs(candidates, candidatesSize, target, returnSize, returnColumnSizes, ret, cur, 0, 0);
+   
+   free(cur); 
+    return ret; 
+}
+
